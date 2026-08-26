3cae488d9b19a48809a81921e37401b5e6146d0d
ajbasa0000 <ajbasa0000@users.noreply.github.com>
2026-08-26 09:41:36 +0800

    Make SSL redirect configurable and support HTTP staging

--- ovpa_website/settings.py
+++ ovpa_website/settings.py
@@ -247,11 +247,11 @@ else:
 
 
 
-# Security Settings (Uncomment for production)
+# Security Settings
 if not DEBUG:
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
+    SECURE_SSL_REDIRECT = config('SECURE_SSL_REDIRECT', default=False, cast=bool)
+    SESSION_COOKIE_SECURE = config('SESSION_COOKIE_SECURE', default=False, cast=bool)
+    CSRF_COOKIE_SECURE = config('CSRF_COOKIE_SECURE', default=False, cast=bool)
     SECURE_BROWSER_XSS_FILTER = True
     SECURE_CONTENT_TYPE_NOSNIFF = True
     X_FRAME_OPTIONS = 'DENY'
